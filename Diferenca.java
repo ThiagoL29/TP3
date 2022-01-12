1fdb03a6e1df5d28656c72b189acb155febe5b52
ThiagoL29 <93353694+ThiagoL29@users.noreply.github.com>
2022-01-12 11:41:06 -0300

    Update Diferenca.java

--- Diferenca.java
+++ Diferenca.java
@@ -1,19 +1,16 @@
 public class Diferenca<T> extends Conjunto{
+    
     private Conjunto tConjunto;
     private Conjunto conjunto;
-
-
-
+    
     public <T> Diferenca(Conjunto<T> tConjunto, Conjunto<T> conjunto) {
-
+        
         this.tConjunto = tConjunto;
         this.conjunto = conjunto;
-
     }
 
     @Override
     public Boolean contemElemento(Object elemento) {
         return ((tConjunto.contemElemento(elemento)) && !(conjunto.contemElemento(elemento)));
     }
-
 }
